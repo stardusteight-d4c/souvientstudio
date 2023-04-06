5347094612502128f954ad0de5df736dbd179dda
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-04-06 11:01:49 -0300

    cards section

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react'
+
 export default function Navbar() {
+  const [isGradientShadowOn, setIsGradientShadowOn] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handlegradientShadowOnScroll)
+    // executeAnimation();
+    return () => {
+      window.removeEventListener('scroll', handlegradientShadowOnScroll)
+    }
+  }, [])
+
+  function handlegradientShadowOnScroll() {
+    if (window.scrollY >= 100) {
+      setIsGradientShadowOn(true)
+    } else if (window.scrollY === 0) {
+      setIsGradientShadowOn(false)
+      // executeAnimation();
+    } else {
+      setIsGradientShadowOn(false)
+    }
+  }
+
   return (
-    <nav className="fixed px-8 z-[1000] w-screen overflow-hidden inset-x-0 top-0 bg-gradient-to-b from-[#020202] via-[#020202]/90 to-transparent">
+    <nav
+      className={`${
+        isGradientShadowOn &&
+        'bg-gradient-to-b from-[#020202] via-[#020202]/90 to-transparent'
+      } fixed px-8 z-[1000] w-screen overflow-hidden inset-x-0 top-0`}
+    >
       <div className="flex items-center max-w-[1200px] mx-auto justify-between py-8 ">
         <h2 className="font-bold text-[20px] tracking-[-1px] uppercase">
           Fevient
--- src/pages/components/Projects.tsx
+++ src/pages/components/Projects.tsx
@@ -1,25 +1,199 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import anime from 'animejs'
+import {
+  jordan,
+  arca,
+  sushiRestaurant,
+  twentyOnePilots,
+  staart,
+  finbook,
+  mba,
+  finalgirl,
+  hellfire,
+} from '../../assets'
 
 export default function Projects() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+  useEffect(() => {
+    if (mounted) {
+      handleObserver()
+    }
+  }, [mounted])
+
+  const projectsSection = useRef<HTMLDivElement>(null)
+  const boxes = [projectsSection]
+
+  function handleObserver() {
+    boxes.forEach((box) => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            if (entry.target.id === 'projects') {
+              anime({
+                targets: `#${entry.target.id}`,
+                translateY: [200, 0],
+                duration: 5000,
+              })
+            }
+          }
+        })
+      })
+      observer.observe(box.current!)
+      return () => observer.disconnect()
+    })
+  }
+
   return (
-    <section className="py-[100px] px-4 relative h-fit overflow-hidden">
+    <section
+      ref={projectsSection}
+      id="projects"
+      className="py-[100px] px-4 relative h-fit "
+    >
       <div className="grid grid-cols-2 gap-8 w-fit mx-auto">
-        <div className="card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img src={jordan.src} alt="" className="w-full h-full object-cover" />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Jordan</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Illustration and Composition
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img src={arca.src} alt="" className="w-full h-full object-cover" />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Jordan</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Illustration and Composition
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
           <img
-            src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7f0435164108103.63f117ed6c2b4.png"
+            src={sushiRestaurant.src}
             alt=""
             className="w-full h-full object-cover"
           />
           <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
           <div className="content">
             <div className="title px-4 text-center">
-              <span className='font-semibold text-2xl uppercase'>Jordan</span>
+              <span className="font-semibold text-2xl uppercase">Jordan</span>
               <br />
-              <span className='font-medium tracking-widest uppercase text-lg'>Illustration and Composition</span>
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Illustration and Composition
+              </span>
             </div>
           </div>
         </div>
-        
+
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img
+            src={twentyOnePilots.src}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Jordan</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Illustration and Composition
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[1032px] h-[250px] group col-span-2 relative">
+          <img src={staart.src} alt="" className="w-full h-full object-cover" />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Staart</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Visual Identity
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img
+            src={hellfire.src}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Staart</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Visual Identity
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img src={mba.src} alt="" className="w-full h-full object-cover" />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Staart</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Visual Identity
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img
+            src={finbook.src}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Staart</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Visual Identity
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="card-animate card border-[3px] border-white/10 overflow-hidden transition-all duration-500 cursor-pointer w-[500px] h-[500px] group col-span-1 relative">
+          <img
+            src={finalgirl.src}
+            alt=""
+            className="w-full h-full object-cover"
+          />
+          <b className="inset-x-0 transition-all bg-gradient-to-t from-[#020202]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
+          <div className="content">
+            <div className="title px-4 text-center">
+              <span className="font-semibold text-2xl uppercase">Staart</span>
+              <br />
+              <span className="font-medium tracking-widest uppercase text-lg">
+                Visual Identity
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
