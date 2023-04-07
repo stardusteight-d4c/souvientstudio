f2074acc03ecb73cddc4cddd51d5740f05c4cf55
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-04-07 19:09:27 -0300

    settings

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -52,7 +52,7 @@ export default function Projects() {
       className="py-[100px] px-4 relative h-fit "
     >
       <div className="md:grid max-w-[1200px] flex flex-col grid-cols-2 gap-8 w-fit mx-auto">
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img src={jordan.src} alt="" className="w-full h-full object-cover" />
           <b className="inset-x-0 transition-all bg-gradient-to-t from-[#FE9BBA]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
           <div className="content">
@@ -66,7 +66,7 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img src={arca.src} alt="" className="w-full h-full object-cover" />
           <b className="inset-x-0 transition-all bg-gradient-to-t from-[#FE9BBA]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
           <div className="content">
@@ -79,7 +79,7 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img
             src={sushiRestaurant.src}
             alt=""
@@ -97,7 +97,7 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img
             src={twentyOnePilots.src}
             alt=""
@@ -115,7 +115,7 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[1032px] h-[250px] group col-span-2 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[1032px] h-[250px] group col-span-2 relative">
           <img src={staart.src} alt="" className="w-full h-full object-cover" />
           <b className="inset-x-0 transition-all bg-gradient-to-t from-[#FE9BBA]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
           <div className="content">
@@ -129,7 +129,7 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img
             src={hellfire.src}
             alt=""
@@ -146,7 +146,7 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img src={mba.src} alt="" className="w-full h-full object-cover" />
           <b className="inset-x-0 transition-all bg-gradient-to-t from-[#FE9BBA]/90 to-transparent bottom-0 h-[100px] hidden group-hover:block"></b>
           <div className="content">
@@ -160,7 +160,7 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img
             src={finbook.src}
             alt=""
@@ -177,7 +177,7 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className="card-animate card border-[10px] border-[#FE9BBA] overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
+        <div className="card-animate card rounded-3xl overflow-hidden transition-all duration-500 cursor-pointer w-full max-w-[500px] h-[500px] group col-span-1 relative">
           <img
             src={finalgirl.src}
             alt=""
